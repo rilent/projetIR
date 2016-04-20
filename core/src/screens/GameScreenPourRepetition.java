8bc1562bb0548014441a31ce95ed67e064a890e9
P Nico <nico.pr@live.fr>
2016-04-20 22:52:28 +0200

    compile, manque encore le ralenti + modif arbres

---
+++ core/src/screens/GameScreenPourRepetition.java
@@ -0,0 +1,136 @@
+package screens;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.math.Vector2;
+import com.mygdx.spaceinvaders.MyGdxspaceinvaders;
+
+import arbres.BinaryTree;
+import controller.WorldControllerMano;
+import model.JoueurAI;
+import model.TankJoueur;
+import model.World;
+import utils.Constants;
+import utils.Individu;
+import view.WorldRenderer;
+
+public class GameScreenPourRepetition extends AbstractGameScreen{
+	
+	MyGdxspaceinvaders gam;
+	WorldRenderer worldRenderer;
+	WorldControllerMano worldController;
+	private World world;
+	private OrthographicCamera camera;
+	int compteur;
+	private int instance = 0; //permets de compter le nombre d'individu, on s'arrête à 50
+	//Hashtable<Integer, BinaryTree> population = new Hashtable<Integer, BinaryTree>(); //on stocke chaque individu et son arbre
+	ArrayList<Individu> population = new ArrayList<Individu>(); //on stocke chaque individu et son arbre
+	
+	
+	
+	public GameScreenPourRepetition(MyGdxspaceinvaders game, int instance, ArrayList<Individu> population) {
+		super(game);
+		this.instance = instance;
+		this.population = population;
+		this.gam =  game;
+		
+		//population.get(instance).getTree()
+		world = new World(new JoueurAI(new TankJoueur(new Vector2(Constants.POSITION_DEPART_TANK_X,Constants.POSITION_DEPART_TANK_Y), new Vector2(1,0))),population.get(instance).getTree());
+		worldRenderer = new WorldRenderer(world);
+		worldController = new WorldControllerMano(world);
+		camera = new OrthographicCamera();
+	}	
+
+
+	@Override
+	public void render(float deltaTime) {
+		this.compteur++;
+		//System.out.println(compteur);
+		//System.out.println(world.getTank().gethitBox().x);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		Gdx.gl.glClearColor(0, 0, 0, 0);
+		worldController.update(deltaTime);
+		worldRenderer.render(deltaTime);
+		world.setCpt_render(world.getCpt_render()+1);
+
+		
+		if(world.isPartieTermine() == true)
+		{
+
+			population.get(instance).setScore(world.getScorePartie());
+			instance = instance + 1;			
+			if(getInstance() < 5)
+			{
+				gam.setScreen(new GameScreenPourRepetition(gam, instance, population));
+			}
+			else
+			{
+				gam.setScreen(new EndingScreen(gam, population));
+			}
+		}
+		
+		
+	}
+	
+	
+	
+	
+
+	public int getInstance() {
+		return instance;
+	}
+
+
+	public void setInstance(int instance) {
+		this.instance = instance;
+	}
+
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+	
+
+	
+	
+}
